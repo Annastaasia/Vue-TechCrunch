522be80c5fe13c7b0d3448d82c2942d3a18f7297
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-11-05 20:20:44 +0300

    feat: create app

---
+++ tech/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <select v-model="selectedTab">
+      <option value="techcrunch">TechCrunch</option>
+      <option value="starwars">StarWars</option>
+    </select>
+    <hr />
+    <TechCrunch v-if="selectedTab === 'techcrunch'" />
+    <StarWars v-if="selectedTab === 'starwars'" />
+  </div>
+</template>
+
+<script>
+import TechCrunch from "./components/TechCrunch.vue";
+import StarWars from "./components/StarWars.vue";
+
+export default {
+  name: "App",
+  components: {
+    TechCrunch,
+    StarWars,
+  },
+  data() {
+    return {
+      selectedTab: "techcrunch",
+    };
+  },
+
+  watch: {
+    selectedTab() {
+      window.location.hash = "";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
